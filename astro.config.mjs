8c9b0281bae1128e1a62326e5f450a74834c0f5d
GastonDeSade <gastondesade@proton.me>
2025-11-07 14:58:23 +0100

    fix: Link path

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://GastonDeSade.github.io',
-	base: '/blog',
+  	base: isProd ? '/blog' : '/',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		shikiConfig: {
